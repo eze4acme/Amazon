4155148b08b3ead4a2b2dec9d0fc7262b9708d7c
Eze Onyebuchi <eze4acme@gmail.com>
2023-09-03 10:55:30 +0100

    update

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,1 @@
-
-let cart = []
-const btns = [... document.querySelectorAll(".one")]
-// console.log(btns);
-btns.forEach((btn) => {
-   btn.addEventListener('click', () => {
-    const name = btn.dataset.class 
-    cart.push({name, qty: 4})
-    console.log(cart);
-   })
-})
+const cart = []
